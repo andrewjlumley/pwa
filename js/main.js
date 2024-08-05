ffa78890ecc601cfe03c2b8e6355a5e9b213b1cb
Andrew Lumley <alumley@uk.gestamp.com>
2024-08-05 15:23:34 +0100

    Complete setup of PWA with image upload to API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,136 @@
-// Make sure server work is supported
+// The width and height of the captured photo. We will set the
+// width to the value defined here, but the height will be
+// calculated based on the aspect ratio of the input stream.
+  
+const width = 320; // We will scale the photo width to this
+let height = 0; // This will be computed based on the input stream
+  
+// |streaming| indicates whether or not we're currently streaming
+// video from the camera. Obviously, we start at false.
+  
+let streaming = false;
+  
+// The various HTML elements we need to configure or control. These
+// will be set by the startup() function.
+  
+let video = null;
+let canvas = null;
+let photos = null;
+let startbutton = null;
+
+let isNetwork = false;
+
+function UpdateNetworkStatus(isActive) {
+    isNetwork = isActive;
+    onlineStatus.textContent = isNetwork ? "online" : "offline";
+}
+
+function showViewLiveResultButton() {
+    if (window.self !== window.top) {
+      // Ensure that if our document is in a frame, we get the user
+      // to first open it in its own tab or window. Otherwise, it
+      // won't be able to request permission for camera access.
+      document.querySelector(".contentarea").remove();
+      const button = document.createElement("button");
+      button.textContent = "View live result of the example code above";
+      document.body.append(button);
+      button.addEventListener("click", () => window.open(location.href));
+      return true;
+    }
+
+    return false;
+}
+    
+function startup() {
+    if (showViewLiveResultButton()) {
+      return;
+    }
+    video = document.getElementById("video");
+    photos = document.getElementById("photos");
+    canvas = document.getElementById("canvas");
+    startbutton = document.getElementById("startbutton");
+    onlineStatus = document.getElementById("status");
+    
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+    
+    video.addEventListener(
+      "canplay",
+      (ev) => {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+    
+          // Firefox currently has a bug where the height can't be read from
+          // the video, so we will make assumptions if this happens.
+    
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+    
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false,
+    );
+    
+    startbutton.addEventListener(
+      "click",
+      (ev) => {
+        TakePicture();
+        ev.preventDefault();
+      },
+      false,
+    );
+  
+    UpdateNetworkStatus(window.navigator.onLine);
+}
+
+async function TakePicture() {
+    const context = canvas.getContext("2d");
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      const data = canvas.toDataURL("image/png");
+  
+      const img = new Image();
+      img.src = data;
+      img.id = "photo";
+      photos.append(img);
+  
+      try {
+        //const url = "http://localhost:1792/api/articles/4181f4d5-961e-4243-bbb8-0b3ac004ac4b/image";
+        const url = "http://researchonionarchitecturetest/api/articles/4181f4d5-961e-4243-bbb8-0b3ac004ac4b/image";
+    
+        // Use postMessage to send data to service worker as registering a sync directly
+        // is problematic if data is large.
+        console.log('Send picture to service worker via message');
+        const registration = await navigator.serviceWorker.ready;
+        registration.active.postMessage({ type: 'sync', url : url, method: 'POST', body: data });
+      }
+
+      catch (err) {
+        console.log(err);
+      }
+    }
+}
+
+window.addEventListener("load", startup, false);
+
+window.addEventListener("online", () => UpdateNetworkStatus(true));
+window.addEventListener("offline", () => UpdateNetworkStatus(false));
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
@@ -11,30 +143,14 @@ if ('serviceWorker' in navigator) {
 
 if ('Notification' in window) {
     window.addEventListener('load', () => {
-        Notification.requestPermission()
-        .then(reg => { 
-            if (reg != 'granted')
-                console.log('Notification permission not granted');
-            else
-                new Notification("Hello, and welcome")
-        })
-        .catch(err => console.log(`The error is ${err}`));
-    });
-}
+      console.log('Notification supported')
 
-async function SendAPI() {
-    console.log('API triggered.');
-
-    let package = {
-        type: "fetch-sync-" + Math.random().toString(16).slice(2),
-        url : "https://httpbin.org/anything",
-        body: {
-            "action": "createOrder",
-            "reference": "TEST AJL 03/08/24 01"
-        }
-    };
-    
-    const registration = await navigator.serviceWorker.ready;
-    registration.sync.register(JSON.stringify(package));
-}
-
+      Notification.requestPermission()
+      .then(reg => {
+          console.log('Notification permission: ' + reg);
+          if (reg != 'granted')
+              console.log('Notification permission not granted');
+      })
+    .catch(err => console.log(`The error is ${err}`));
+  });
+};
--- sw.js
+++ sw.js
@@ -1,19 +1,23 @@
 importScripts('js/utilities.js');
 
+// https://medium.com/@a7ul/beginners-guide-to-web-push-notifications-using-service-workers-cb3474a17679
+// https://www.youtube.com/watch?v=ksXwaWHCW6k
+
 const cacheName = 'v1';
 
-/* const cacheAssets = [
+const cacheAssets = [
     'index.html',
     'about.html',
     '/js/main.js',
     'sw.js'
-]; */
+];
 
-// Call install event
+// Create 'Install' event on creation of service worker.
 self.addEventListener('install', e => {
     console.log('Service worker installed.');
 
- /*    e.waitUntil(
+    // Lets force the caching of major files
+    e.waitUntil(
         caches
             .open(cacheName)
             .then(cache => {
@@ -21,11 +25,13 @@ self.addEventListener('install', e => {
                 cache.addAll(cacheAssets);
             })
             .then(() => self.skipWaiting())
-    ); */
+    );
 });
 
+// Create 'Activate' event on awakening of service worker.
 self.addEventListener('activate', async (e) => {
     console.log('Service worker activated.');
+    
     // Remove unwanted caches
     e.waitUntil(
         caches.keys().then(cacheNames => {
@@ -39,45 +45,29 @@ self.addEventListener('activate', async (e) => {
             )
         })
     );
-    //
+
+    // Lets subscribe for remote notifications
     try {
         const applicationServerKey = urlB64ToUint8Array(
             'BEkQr_98A7QLRZA5laJY12BkfvvGezj1N7tssavYWABBswy49JDqYdnF6zrZoMv4EjYZCEBOW8fOOaOz_2WR-IY'
         )
         const options = { applicationServerKey, userVisibleOnly: true }
         const subscription = await self.registration.pushManager.subscribe(options)
+        
+        // retrieve subscription details and pass to backend to trigger notifications
         console.log(JSON.stringify(subscription))
         console.log(subscription.endpoint);
         console.log(subscription.toJSON().keys.p256dh);
-        console.log(subscription.toJSON().keys.auth);
-
-        // give this information to the backend so it can send a message out
-        
+        console.log(subscription.toJSON().keys.auth);       
     } catch (err) {
         console.log('Error', err)
-    }
-
-    // https://medium.com/@a7ul/beginners-guide-to-web-push-notifications-using-service-workers-cb3474a17679
-    
+    } 
 });
 
-
-
-
-// Call fetch event
-/* self.addEventListener('fetch', e => {
-    console.log('Service worker fetching');
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    );
-}); */
-
-// https://www.youtube.com/watch?v=ksXwaWHCW6k
-
+// Create 'Fetch' event to handle requests from browser.
+// This should only handle GET!
 self.addEventListener('fetch', e => {
     console.log('Service worker fetching');
-    console.log('Method: ' + e.request.method);
-
 
     e.respondWith(
         fetch(e.request)
@@ -96,28 +86,56 @@ self.addEventListener('fetch', e => {
     );
 });
 
+// Create 'Push' event to handle notifications and throw to browser
 self.addEventListener('push', function(event) {
     if (event.data) {
         console.log(event.data.text());
         const title = "Something Has Happened";
-        const message = event.data
+        const message = event.data.text();
         self.registration.showNotification(title, { Body: message});
     } else {
       console.log('Push event but no data')
     }
 });
 
+// Create 'Message' event to handle PUT, POST and DELETE requests from browser
+// We then regiser a sync request event to handle this.
+const syncStore = {}         
+self.addEventListener('message', event => {
+    console.log("Message received: " + event.data);
+    if(event.data.type === 'sync') {
+      // get a unique id to save the data
+      let id = "fetch-push-article-photo-sync-" + Math.random().toString(16).slice(2);
+      syncStore[id] = event.data
+      // register a sync and pass the id as tag for it to get the data
+      self.registration.sync.register(id)
+    }
+    console.log(event.data)
+});
+
+// Create 'Sync' event to handle posts to external API. Data should contain
+// Url and body.
 self.addEventListener('sync', async (e) => {
     console.log(e.tag);
-    const receivedJsonTag = JSON.parse(e.tag);
-    console.log(receivedJsonTag.body);    
-    
-    let response = await fetch(receivedJsonTag.url, {
-        method: 'POST',
-        body: JSON.stringify(receivedJsonTag.body),
+
+    let json = syncStore[e.tag];
+    console.log(json);
+
+    var blob = await fetch(json.body).then(res => res.blob());
+
+    const formData = new FormData();
+    formData.append('file', blob, 'filename.png');
+
+    // Call fetch, ensuring Network credential are included
+    let response = await fetch(json.url, {
+        method: json.method,
+        body: formData,
+        mode: 'no-cors',
+        credentials: 'include',
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
-        }
+          'Authorization': 'Negotiate'
+        },
     })
     .then(function(response) { 
         return response.text();
@@ -125,5 +143,6 @@ self.addEventListener('sync', async (e) => {
     })
     .catch(error => console.error('Error:', error));
 
-    console.log(response);
+    console.log("Fetch response: " + response);
+    self.registration.showNotification("Submitted");
 });
